307a402a4567182afa48c52aeeef27cb6e6cd1c4
Camila Guimarães <camiilacguimaraes@gmail.com>
2023-06-17 18:57:20 -0300

    :ok_hand: alteração da indentação

--- moveit/src/components/Countdown.tsx
+++ moveit/src/components/Countdown.tsx
@@ -1,68 +1,70 @@
 //useState => funcionalidade para definir estados dentro do componente
 //useEffect => funcionalidade para disparar efeitos colaterais (quando algo acontecer um efeito e gerado)
 import { useContext } from 'react';
+
 import { CountdownContext } from '../contexts/CountdownContext';
+
 import styles from '../styles/components/Countdown.module.css';
 
 export function Countdown() {
-    const { 
-        minutes, 
-        seconds, 
-        hasFinished,
-        isActive,
-        startCountdown,
-        resetCountdown
-    } = useContext(CountdownContext);
+  const { 
+    minutes, 
+    seconds, 
+    hasFinished,
+    isActive,
+    startCountdown,
+    resetCountdown
+  } = useContext(CountdownContext);
 
-    //essas variaveis não foram para o context pq a formatação faz parte do layout da aplicação
-    //se a string não tiver dois caracteres o padStart coloca 0 na primeira posição
-    const [minuteLeft, minuteRight]  = String(minutes).padStart(2, '0').split('');
-    const [secondLeft, secondRight]  = String(seconds).padStart(2, '0').split('');
+  //essas variaveis não foram para o context pq a formatação faz parte do layout da aplicação
+  //se a string não tiver dois caracteres o padStart coloca 0 na primeira posição
+  const [minuteLeft, minuteRight]  = String(minutes).padStart(2, '0').split('');
+  const [secondLeft, secondRight]  = String(seconds).padStart(2, '0').split('');
 
-    return (
+  return (
+    <div>
+      <div className={ styles.countdownContainer }>
         <div>
-            <div className={ styles.countdownContainer }>
-                <div>
-                    <span>{ minuteLeft }</span> {/*tem um separação entre os numeros e não fica tão facil tratar se tiver tudo junto*/}
-                    <span>{ minuteRight }</span>
-                </div>      
-                <span>:</span>
-                <div>
-                    <span>{ secondLeft }</span>
-                    <span>{ secondRight }</span>
-                </div>
-            </div>
-
-            {/*quando está dento de chave indica um codigo em*/}
-            { hasFinished ? (
-                <button 
-                    disabled
-                    className={ `${styles.countdownButton} ${styles.countdownButtonCheck}` }
-                >               
-                    Ciclo encerrado
-                    <img src='icons/check_circle.svg' />
-                </button>
-            ) : (
-               <>
-                    { isActive ? (
-                        <button 
-                            type="button" 
-                            className={ `${styles.countdownButton} ${styles.countdownButtonActive}` }
-                            onClick={ resetCountdown }
-                        >               
-                            Abondanar ciclo
-                        </button>
-                    ) : (
-                        <button 
-                            type="button" 
-                            className={ styles.countdownButton }
-                            onClick={ startCountdown }
-                        >               
-                            Iniciar um ciclo
-                        </button>
-                    )}      
-               </> 
-            )}              
+          <span>{ minuteLeft }</span> {/*tem um separação entre os numeros e não fica tão facil tratar se tiver tudo junto*/}
+          <span>{ minuteRight }</span>
+        </div>      
+        <span>:</span>
+        <div>
+          <span>{ secondLeft }</span>
+          <span>{ secondRight }</span>
         </div>
-    );
+      </div>
+
+      {/*quando está dento de chave indica um codigo em*/}
+      { hasFinished ? (
+        <button 
+          disabled
+          className={ `${styles.countdownButton} ${styles.countdownButtonCheck}` }
+        >               
+          Ciclo encerrado
+          <img src='icons/check_circle.svg' />
+        </button>
+      ) : (
+        <>
+          { isActive ? (
+            <button 
+              type="button" 
+              className={ `${styles.countdownButton} ${styles.countdownButtonActive}` }
+              onClick={ resetCountdown }
+            >               
+              Abondanar ciclo
+            </button>
+          ) : (
+            <button 
+              type="button" 
+              className={ styles.countdownButton }
+              onClick={ startCountdown }
+            >               
+              Iniciar um ciclo
+            </button>
+          )}      
+        </> 
+      )}              
+    </div>
+  );
 }
